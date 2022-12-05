42cef3fa9e1150c2861643bd4531278ea35ca2ca
vamshikrishna1997 <86228152+vamshikrishna1997@users.noreply.github.com>
2022-12-05 18:30:46 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
-ver=`curl --silent http://mirror.vorboss.net/apache/tomcat/tomcat-9/ | grep v9 | awk '{split($5,c,">v") ; split(c[2],d,"/") ; print d[1]}'`
-sudo yum update -y
-sudo yum install -y java wget git
-cd /opt
-rm -rf apache*
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
-sudo tar xvzf apache-tomcat-9.*
-sudo rm -rf apache*.gz
-sudo git clone https://github.com/adhig93/tomcat-config
-sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
-sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
-sudo cp ./tomcat-config/tomcat-users.xml /opt/apache-tomcat-9.*/conf/tomcat-users.xml
-sudo rm -rf tomcat-config
-cd /opt/apache-tomcat-9.*
-sudo sh bin/startup.sh
+#/bin/bash
+yum update -y 
+yum install -y wget java-11-openjdk-devel
+cd /usr/local
+wget https://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.62/bin/apache-tomcat-9.0.62.tar.gz
+tar -xvf apache-tomcat-9.0.62.tar.gz
+mv apache-tomcat-9.0.62 tomcat
+echo "export CATALINA_HOME="/usr/local/tomcat9"" >> ~/.bashrc
+source ~/.bashrc
+sh /usr/local/tomcat9/bin/startup.sh
