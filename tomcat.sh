b28e0c3679c3134b98c7a0914816616379be6e6a
vamshikrishna1997 <86228152+vamshikrishna1997@users.noreply.github.com>
2022-12-06 18:35:06 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -4,7 +4,7 @@ yum install -y wget java-11-openjdk-devel
 cd /usr/local
 wget https://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.62/bin/apache-tomcat-9.0.62.tar.gz
 tar -xvf apache-tomcat-9.0.62.tar.gz
-mv apache-tomcat-9.0.62 tomcat11
-echo "export CATALINA_HOME="/usr/local/tomcat11"" >> ~/.bashrc
+mv apache-tomcat-9.0.62 tomcat9
+echo "export CATALINA_HOME="/usr/local/tomcat9"" >> ~/.bashrc
 source ~/.bashrc
-sh /usr/local/tomcat11/bin/startup.sh
+sh /usr/local/tomcat9/bin/startup.sh
